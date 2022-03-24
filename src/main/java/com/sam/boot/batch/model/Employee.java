35a9513df42be4abdb3da477e9a6b1552a4dba67
samckp <sam.mca.liet@gmail.com>
2022-03-24 08:35:48 +0530

    updated

--- src/main/java/com/sam/boot/batch/model/Employee.java
+++ src/main/java/com/sam/boot/batch/model/Employee.java
@@ -1,11 +1,13 @@
 package com.sam.boot.batch.model;
 
-public class Product {
+public class Employee {
 
     private Integer id;
-    private String name;
-    private String description;
-    private Double price;
+    private String firstname;
+    private String lastname;
+    private String company;
+    private String address;
+    private Double salary;
 
     public Integer getId() {
         return id;
@@ -15,27 +17,43 @@ public class Product {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
     }
 
-    public String getDescription() {
-        return description;
+    public String getLastname() {
+        return lastname;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 
-    public Double getPrice() {
-        return price;
+    public String getCompany() {
+        return company;
     }
 
-    public void setPrice(Double price) {
-        this.price = price;
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
     }
 }
